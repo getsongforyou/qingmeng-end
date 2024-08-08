4c0c90bdb201f1c79594b7f43a06d59392c36ab6
moyi <2012524820@qq.com>
2024-08-08 15:16:52 +0800

    token验证

--- app.js
+++ app.js
@@ -3,17 +3,28 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const jwt = require('jsonwebtoken')
-let { expressjwt} = require('express-jwt')
+let { expressjwt } = require('express-jwt')
 
-app.use(cors({origin: '*'}))
+app.use(cors({ origin: '*' }))
 
 app.use(expressjwt({
   secret: '8888',
   algorithms: ['HS256'],
-}).unless({path: ['/login']}))
+  // getToken: function fromHeaderOrQuerystring(req) {
+  //     if (
+  //       req.headers.authorization &&
+  //       req.headers.authorization.split(" ")[0] === "Bearer"
+  //     ) {
+  //       return req.headers.authorization.split(" ")[1];
+  //     } else if (req.query && req.query.token) {
+  //       return req.query.token;
+  //     }
+  //     return null;
+  //   }
+}).unless({ path: ['/login'] }))
 
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 
 
 const connection = mysql.createConnection({
@@ -21,59 +32,67 @@ const connection = mysql.createConnection({
   user: 'root',
   password: '12345600',
   database: 'qingmeng'
-})    
+})
 connection.connect()
 
 // login  put first  ************************
-app.post('/login',(req, res)=> {
-    const {name, password}  = req.body
-    
-
-    const sql = `select * from user where username = '${name}' and password = ${password};`
-    connection.query(sql,(err, rows, fields)=>{
-      if(err)throw err;
-      console.log(rows)
-      if(rows.length>0){
-        const token = 'Bearer ' + jwt.sign(
-          {
-            name
-          },
-          '8888',
-          {
-            expiresIn: 3600*24*7
-          }
-        )
-        res.send({
-          success: true,
-          message: '成功登录',
-          token: token
-        })  
-        console.log(`${name}登录成功`)
-      }else{
-        res.send({
-          success: false,
-          message: '登录失败'
-        })  
-      }  
-
-    } )  
-})    
+app.post('/login', (req, res) => {
+  const { username, password } = req.body
 
 
+  const sql = `select * from user where username = '${username}' and password = '${password}';`
+  connection.query(sql, (error, rows, fields) => {
+    if (error) throw error;
+    console.log(rows)
+    if (rows.length > 0) {
+      const token = 'Bearer ' + jwt.sign(
+        {
+          username
+        },
+        '8888',
+        {
+          expiresIn: 3600 * 24 * 7
+        }
+      )
+      res.send({
+        success: true,
+        message: '成功登录',
+        token: token
+      })
+      console.log(`${username}登录成功`)
+    } else {
+      res.send({
+        success: false,
+        message: '登录失败'
+      })
+    }
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+  })
+  
+})
 
-
-
-
-app.get('/getinfo',(req, res)=>{
-res.send()
+app.get('/getInfo', (req, res) => {
+  console.log('/getinfo')
+  console.log(req.auth)
+  connection.query(`select role from user where username ='${req.auth.username}';`, (error, rows) => {
+    if (error) throw error;
+    console.log(rows)
+    if(rows.length>0){
+      console.log(rows, 'role')
+      res.send({
+        username: req.auth.username,
+        role: rows[0].role
+      })
+    }else{
+      res.send('error')
+    }
+  })
+  // res.send()sb，害我浪费半天时间
 })
 
 
 
-app.get('/getTable', (req, res)=>{
+app.get('/getTable', (req, res) => {
   console.log('gettable')
   res.send({
     tableData: [{
@@ -93,11 +112,12 @@ app.get('/getTable', (req, res)=>{
       name: '王小虎',
       address: '上海市普陀区金沙江路 1516 弄'
     }]
-  })  
+  })
 })
 
 app.get('/getTree', (req, res) => {
-  res.send({tree: [{
+  res.send({
+    tree: [{
       id: 1,
       label: '一级 1',
       children: [{
@@ -131,15 +151,16 @@ app.get('/getTree', (req, res) => {
         id: 8,
         label: '二级 3-2'
       }]
-    }]})
+    }]
+  })
 })
 
-app.use((err, req, res, next)=>{
-  res.send({err})
+app.use((err, req, res, next) => {
+  res.send({ err })
 })
 
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
   console.log('run on http://127.0.0.1:3000')
 })
 
--- test.js
+++ test.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const jwt = require('jsonwebtoken')
-const {expressjwt} = require('express-jwt')
+const { expressjwt } = require('express-jwt')
 const cors = require('cors')
 const app = express()
+
+
+
+const res = jwt.sign({ username: 'aaa'}, 'key', { expiresIn: 3600 })
+console.log(res)
+jwt.verify(res, 'key', (err, data) => {
+    console.log('start')
+    if(err)return console.log(err)
+
+        console.log(data)
+})
+
+
+
 
 app.use(cors())
 
@@ -10,23 +24,23 @@ app.use(expressjwt({
     secret: '8888',
     algorithms: ['HS256']
 }).unless({
-    path: ["/login"]
+    path: ["/"]
 }))
 
-app.post('/login', (req,res)=>{
+app.post('/', (req, res) => {
     const userInfo = {
         _id: '33',
         username: 'fly'
     }
-    
-    const resc = jwt.sign(userInfo, 'key', {expiresIn: 3600})
-    const token = 'Bearer'+resc
-    res.send({message:'success', token})
+
+    const resc = jwt.sign(userInfo, 'key', { expiresIn: 3600 })
+    const token = 'Bearer' + resc
+    res.send({ message: 'success', token })
 })
 
 
-app.use((err, req, res, next)=>{
-    if(err)res.send(err)
+app.use((err, req, res, next) => {
+    if (err) res.send(err)
 })
 
-app.listen(3000, ()=>console.log('run at http://127.0.0.1:3000'))
+app.listen(3000, () => console.log('run at http://127.0.0.1:3000'))
